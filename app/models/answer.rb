d757c9b2f010d0aea44672540ee54032d496e243
pavel-sazonov <wismyl@ya.ru>
2018-04-24 17:37:45 +0300

    fix mistakes

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,6 @@
 class Answer < ApplicationRecord
   belongs_to :question
 
-  validates :question_id, presence: true
   validates :body, length: { minimum: 30 }, allow_nil: true
+  validates :body, presence: true
 end
