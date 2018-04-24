68c128368c3eddd5f6bc0d2d897a21ca29c43993
pavel-sazonov <wismyl@ya.ru>
2018-04-24 12:13:03 +0300

    add model Answer

---
+++ app/models/answer.rb
@@ -0,0 +1,6 @@
+class Answer < ApplicationRecord
+  belongs_to :question
+
+  validates :question_id, presence: true
+  validates :body, length: { minimum: 30 }
+end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,7 @@
 class Question < ApplicationRecord
+  has_many :answers, dependent: :destroy
 
-  validates :title, :body, presence: true
-  validates :title, length: { in: 10..30 }
-  validates :body, length: { minimum: 10 }
+  # validates :title, :body, presence: true
+  validates :title, length: { in: 10..50 }
+  validates :body, length: { minimum: 30 }
 end
