8c89a554c8c3358e81ac5bb1905b2f07b2adfcdb
pavel-sazonov <wismyl@ya.ru>
2018-04-25 09:38:24 +0300

    refactoring

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,6 @@
 class Answer < ApplicationRecord
   belongs_to :question
 
-  validates :body, length: { minimum: 30 }, allow_nil: true
   validates :body, presence: true
+  validates :body, length: { minimum: 30 }, allow_nil: true
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
 
+  validates :title, :body, presence: true
   validates :title, length: { in: 10..50 }, allow_nil: true
   validates :body, length: { minimum: 30 }, allow_nil: true
-  validates :title, :body, presence: true
 end
