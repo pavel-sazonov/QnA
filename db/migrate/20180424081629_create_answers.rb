d4ae0889e60d6b27896fcb164d03538a9e212e58
pavel-sazonov <wismyl@ya.ru>
2018-04-25 09:28:40 +0300

    add null: false

--- db/migrate/20180424081629_create_answers.rb
+++ db/migrate/20180424081629_create_answers.rb
@@ -1,7 +1,7 @@
 class CreateAnswers < ActiveRecord::Migration[5.2]
   def change
     create_table :answers do |t|
-      t.text :body
+      t.text :body, null: false
       t.references :question, foreign_key: true
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 2018_04_24_081629) do
   enable_extension "plpgsql"
 
   create_table "answers", force: :cascade do |t|
-    t.text "body"
+    t.text "body", null: false
     t.bigint "question_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -24,8 +24,8 @@ ActiveRecord::Schema.define(version: 2018_04_24_081629) do
   end
 
   create_table "questions", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
+    t.string "title", null: false
+    t.text "body", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
