da28aeff0be8b3c492aafda50a9a8e07114fcbc5
pavel-sazonov <wismyl@ya.ru>
2018-04-29 17:36:00 +0300

    Add answer belongs to user relation

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
+  it { should belong_to(:user) }
   it { should belong_to(:question) }
 
   it { should validate_presence_of :body }
@@ -8,5 +9,6 @@ RSpec.describe Answer, type: :model do
     should validate_length_of(:body).
     is_at_least(5)
   end
+  it { should have_db_index(:user_id) }
   it { should have_db_index(:question_id) }
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -14,4 +14,5 @@ RSpec.describe Question, type: :model do
     should validate_length_of(:body).
     is_at_least(5)
   end
+    it { should have_db_index(:user_id) }
 end
