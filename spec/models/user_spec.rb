fc35ce681a3db7382fe3b0c0b9a2c5b16ee92a9c
pavel-sazonov <wismyl@ya.ru>
2018-05-03 18:00:07 +0300

    Add author_of? in user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,18 @@ RSpec.describe User do
 
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
+
+  describe 'author_of(item)?' do
+    let(:user) { create(:user) }
+    let(:another_user) { create(:user) }
+    let(:question) { create(:question, user: user) }
+
+    it 'Returns true if item belongs to user' do
+      expect(question.user_id).to eq user.id
+    end
+
+    it 'Returns false if item does not belong to user' do
+      expect(question.user_id).to_not eq another_user.id
+    end
+  end
 end
