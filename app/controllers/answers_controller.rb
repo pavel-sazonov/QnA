e52f64e931b788749c4a2366ea046c396f32f6f6
pavel-sazonov <wismyl@ya.ru>
2018-05-03 12:09:13 +0300

    fix AnswersController flaws

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,14 +1,18 @@
 class AnswersController < ApplicationController
-  before_action :authenticate_user!, only: %i[create]
+  before_action :authenticate_user!, only: %i[create destroy]
   before_action :find_question, only: %i[index new create]
 
   def create
     @answer = @question.answers.new(answer_params)
     @answer.user = current_user
 
-    flash[:notice] = 'Your answer successfully created.' if @answer.save
+    if @answer.save
+      flash[:notice] = 'Your answer successfully created.'
 
-    redirect_to @question
+      redirect_to @question
+    else
+      render 'questions/show'
+    end
   end
 
   def destroy
@@ -17,11 +21,11 @@ class AnswersController < ApplicationController
     if current_user.author_of?(@answer)
       @answer.destroy
       flash[:notice] = 'Answer deleted.'
-      redirect_to @answer.question
     else
       flash[:alarm] = 'You can not delete this answer.'
-      redirect_to @answer.question
     end
+
+    redirect_to @answer.question
   end
 
   private
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
-  before_action :authenticate_user!, except: %i[index show]
+  before_action :authenticate_user!, except: %i[index show destroy]
   before_action :load_question, only: %i[show edit update destroy]
 
   def index
