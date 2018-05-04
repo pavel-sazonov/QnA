abcae64d55c3e774f363d27ec8719a242f92f661
pavel-sazonov <wismyl@ya.ru>
2018-05-04 15:44:20 +0300

    Refactor #author_of? spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,17 +7,17 @@ RSpec.describe User do
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
 
-  describe 'author_of(item)?' do
+  describe '#author_of?' do
     let(:user) { create(:user) }
     let(:another_user) { create(:user) }
     let(:question) { create(:question, user: user) }
 
     it 'Returns true if item belongs to user' do
-      expect(user.author_of?(question)).to eq true
+      expect(user).to be_author_of(question)
     end
 
     it 'Returns false if item does not belong to user' do
-      expect(another_user.author_of?(question)).to eq false
+      expect(another_user).to_not be_author_of(question)
     end
   end
 end
