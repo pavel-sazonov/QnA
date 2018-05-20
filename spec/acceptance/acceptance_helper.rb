3eebf0bd750cddb46e66e98fb01e7a24b53be8c1
pavel-sazonov <wismyl@ya.ru>
2018-05-20 13:16:53 +0300

    acceptance_helper.rb

--- spec/acceptance/acceptance_helper.rb
+++ spec/acceptance/acceptance_helper.rb
@@ -3,6 +3,7 @@ require 'capybara/poltergeist'
 
 RSpec.configure do |config|
   Capybara.javascript_driver = :poltergeist
+  Capybara.default_max_wait_time = 5
 
   config.include AcceptanceHelpers, type: :feature
 end
