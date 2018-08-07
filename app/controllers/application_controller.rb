889be3114a56468f329bd2c54432dfd4a85055b5
pavel-sazonov <wismyl@ya.ru>
2018-08-07 11:15:10 +0300

    check_authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,8 @@ class ApplicationController < ActionController::Base
 
   before_action :gon_user
 
+  check_authorization unless: :devise_controller?
+
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, alert: exception.message
   end
